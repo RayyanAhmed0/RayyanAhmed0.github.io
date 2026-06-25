961584a95790528d9ba1f19be45e63751809bc5d
RayyanAhmed0 <Rayyan.Ahmed@jpschools.org>
2026-06-25 15:11:34 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -43,16 +43,11 @@ var init = function (window) {
         and check to see if it has drifted off the screen.         
         */
     function update() {
-      // TODO 4 : Update the position of each circle using physikz.updatePosition()
-      
-      // TODO 5 : Call game.checkCirclePosition() on your circles
-      
-      // TODO 8 / TODO 9 : Iterate over the array
+      for (var i = 0; i < circles.length; i++) {
+        physikz.updatePosition(circles[i]);
+        game.checkCirclePosition(circles[i]);
+      }
     }
-    for (var i = 0; i < circles.length; i++) {
-      physikz.updatePosition(circles[i]);
-    }
-    game.checkCirclePosition(circles[i]);
 
     /* 
         This Function should check the position of a circle that is passed to the 
